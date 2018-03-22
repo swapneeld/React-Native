8816ee0b5b727e6be799722c05e615f9cd9ded52
Swapneelkumar Deshpande <swapneel.deshpande@microsoft.com>
2018-03-22 17:27:24 +0530

    added VSO package to base project

--- Basic_Controls/App.js
+++ Basic_Controls/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Greeting from 'greetingspoc'
+import ReactXpApp from 'helloworldxppoc'
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Simple App connected to App center........!</Text>
         <Greeting name='Rexxar' />
+        <ReactXpApp/>
       </View>
     );
   }
