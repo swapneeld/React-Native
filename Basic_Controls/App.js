0842ead0838f20daaa3301f4a773395547cd3d76
Pratik Bhattacharya <patrickb404@outlook.com>
2018-03-23 18:35:23 +0530

    Added multi tenant choosing capability

--- Basic_Controls/App.js
+++ Basic_Controls/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import codePush from 'react-native-code-push';
+
+let codePushOptions = { 
+  checkFrequency: codePush.CheckFrequency.MANUAL //Make it manual when the check needs to be made only by clicking the button
+}
 
 export default class App extends React.Component {
+  onButtonPressTenant1() {
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE,
+      deploymentKey: 'PQIwZqRp2gp0rQKiJAo0fuVa0TM3BJ5-7LG5f'
+    });
+  }
+  onButtonPressTenant2() {
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE,
+      deploymentKey: '1uPTdk2pQAblXlzZ9Fqu3OK3Wp2UrkPzXIf9G'
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Simple App connected to App center</Text>
+        <Text>Shell App</Text>
+        <TouchableOpacity onPress={this.onButtonPressTenant1}>
+          <Text>Tenant 1 Update</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.onButtonPressTenant2}>
+          <Text>Tenant 2 Update</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -19,3 +44,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+App = codePush(codePushOptions)(App);
---
+++ Basic_Controls/App.tenant2.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import codePush from 'react-native-code-push';
+
+let codePushOptions = { 
+  checkFrequency: codePush.CheckFrequency.MANUAL //Make it manual when the check needs to be made only by clicking the button
+}
+
+export default class App extends React.Component {
+  onButtonPressTenant1() {
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE,
+      deploymentKey: 'PQIwZqRp2gp0rQKiJAo0fuVa0TM3BJ5-7LG5f'
+    });
+  }
+  onButtonPressTenant2() {
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE,
+      deploymentKey: '1uPTdk2pQAblXlzZ9Fqu3OK3Wp2UrkPzXIf9G'
+    });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Customized App for Tenant 2</Text>
+        <TouchableOpacity onPress={this.onButtonPressTenant1}>
+          <Text>Tenant 1 Update</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.onButtonPressTenant2}>
+          <Text>Tenant 2 Update</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+App = codePush(codePushOptions)(App);
