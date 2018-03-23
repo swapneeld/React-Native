e510d6f42ff8920bce9ed150fab8e6631e4c95e3
Pratik Bhattacharya <patrickb404@outlook.com>
2018-03-23 18:51:55 +0530

    Updating minor changes in build.gradle

--- Basic_Controls/android/app/src/main/java/com/basic_controls/MainApplication.java
+++ Basic_Controls/android/app/src/main/java/com/basic_controls/MainApplication.java
@@ -19,10 +19,10 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
 
-        @Override
-        protected String getJSBundleFile() {
-        return CodePush.getJSBundleFile();
-        }
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
     
     @Override
     public boolean getUseDeveloperSupport() {
@@ -33,7 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
+            new CodePush("aIGk2fRfnnx_U8Mrl0GQDinTlnT1Syz6fvCOz", MainApplication.this, BuildConfig.DEBUG)
+            //new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
             new AppCenterReactNativePackage(MainApplication.this)
